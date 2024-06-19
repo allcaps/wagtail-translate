cbb6b7e4fad811be8631d3f21334dbe6185e776a
Coen van der Kamp <coen@fourdigits.nl>
2024-06-19 14:35:04 +0200

    Test patch needed (#9)

--- src/wagtail_translate/apps.py
+++ src/wagtail_translate/apps.py
@@ -1,4 +1,14 @@
+import wagtail
+
 from django.apps import AppConfig
+
+
+def patch_needed(version=wagtail.VERSION) -> bool:
+    """
+    Wagtail 6.2 introduces the `copy_for_translation_done` signal.
+    Older Wagtail versions need to be patched.
+    """
+    return version[0] < 6 or (version[0] == 6 and version[1] < 2)
 
 
 class WagtailTranslateAppConfig(AppConfig):
@@ -7,5 +17,5 @@ class WagtailTranslateAppConfig(AppConfig):
     verbose_name = "Wagtail Translate"
 
     def ready(self):
-        # We patch Wagtail to inject the copy_for_translation_done signal.
-        from . import monkeypatches  # noqa
+        if patch_needed():
+            from . import monkeypatches  # noqa
--- src/wagtail_translate/monkeypatches.py
+++ src/wagtail_translate/monkeypatches.py
@@ -1,14 +1,13 @@
 """
-Ideally, Wagtail would fire a copy_for_translation_done signal.
+Wagtail 6.2 introduces the copy_for_translation_done signal.
+Wagtail Translate will patch older versions of Wagtail.
 
-For now, we monkeypatch the `CopyPageForTranslationAction.walk` and
-`CopyPageForTranslationAction.execute` methods
-to register the copy_for_translation_done signals.
+The `CopyPageForTranslationAction.walk` and
+`CopyPageForTranslationAction.execute` methods are patched.
+The new methods send the copy_for_translation_done signal.
 """
 
 import logging
-
-import wagtail
 
 from wagtail.actions.copy_for_translation import CopyPageForTranslationAction
 
@@ -58,14 +57,11 @@ def new_execute(self, skip_permission_checks=False):
     return translated_page
 
 
-# Wagtail 6.2 introduces the `copy_for_translation_done` signal.
-# Only apply the monkeypatch if Wagtail < 6.2.
-if wagtail.VERSION[0] < 6 or (wagtail.VERSION[0] == 6 and wagtail.VERSION[1] < 2):
-    logger.warning(
-        "Monkeypatching wagtail.actions.copy_for_translation.CopyPageForTranslationAction.walk, send copy_for_translation_done signal"
-    )
-    logger.warning(
-        "Monkeypatching wagtail.actions.copy_for_translation.CopyPageForTranslationAction.execute, send copy_for_translation_done signal"
-    )
-    CopyPageForTranslationAction.walk = new_walk
-    CopyPageForTranslationAction.execute = new_execute
+logger.warning(
+    "Monkeypatching wagtail.actions.copy_for_translation.CopyPageForTranslationAction.walk, send copy_for_translation_done signal"
+)
+logger.warning(
+    "Monkeypatching wagtail.actions.copy_for_translation.CopyPageForTranslationAction.execute, send copy_for_translation_done signal"
+)
+CopyPageForTranslationAction.walk = new_walk
+CopyPageForTranslationAction.execute = new_execute
