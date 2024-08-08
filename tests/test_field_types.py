dbf7da0d0c64f01163c8768deaf179e953a1697a
Coen van der Kamp <coen@fourdigits.nl>
2024-08-08 15:21:40 +0200

    Add tests, improve translation of blocks, add translate related objects

--- tests/test_field_types.py
+++ tests/test_field_types.py
@@ -4,6 +4,7 @@ from django.db import models
 from wagtail.fields import RichTextField
 
 from tests.factories import BlogPostPageFactory, LocaleFactory
+from tests.testapp.models import BlogCategory, BlogPostPage
 
 
 pytestmark = pytest.mark.django_db
@@ -23,3 +24,26 @@ def test_translate_rich_text_field():
     field = translation._meta.get_field("intro")
     assert isinstance(field, RichTextField)
     assert translation.intro == "<p>Uryyb <em>jbeyq</em></p>"
+
+
+def test_translate_fk_snippet():
+    locale_en = LocaleFactory(language_code="en")  # the default language
+    locale_fr = LocaleFactory(language_code="fr")
+    category = BlogCategory.objects.create(name="One Two Three")
+    page = BlogPostPageFactory(title="One", category=category)
+    translated_page = page.copy_for_translation(locale_fr)
+
+    # Since there is NO translation, the original snippet is used.
+    assert translated_page.category == category
+    assert translated_page.category.locale == locale_en
+
+    # Create a translation for the target page, the translated page is used.
+    translated_page.delete()
+    translated_snippet = category.copy_for_translation(locale_fr)
+
+    assert BlogPostPage.objects.count() == 1
+    assert BlogCategory.objects.count() == 2
+
+    translated_page = page.copy_for_translation(locale_fr)
+    assert translated_page.category == translated_snippet
+    assert translated_page.category.locale == locale_fr
